28e6feeb095a93b4850caad4d37658d0291173bc
leighcvgw <leigh.carey@vgw.co>
2020-04-15 21:55:00 +0800

    Allow traffic from ALB to api-target-group

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import * as awsx from "@pulumi/awsx";
 // import { FargateService } from "@lib-server/fargate-service";
 
-const loadBalancer = new awsx.lb.ApplicationLoadBalancer("chu-redemption-spike");
+const serverPort = 8080;
+const sg = new awsx.ec2.SecurityGroup("alb-sg", {
+    // Open egress traffic to api-target-group
+    egress: [{ protocol: "tcp", fromPort: serverPort, toPort: serverPort, cidrBlocks: [ "0.0.0.0/0" ], description: "Allow traffic to api-target-group" }],
+});
+
+const loadBalancer = new awsx.lb.ApplicationLoadBalancer("chu-redemption-spike", { securityGroups: [sg] });
 const listener = loadBalancer.createListener("alb-listener", { port: 80 });
 
-const apiTargetGroup = loadBalancer.createTargetGroup("api-target-group", { port: 8080 });
+const apiTargetGroup = loadBalancer.createTargetGroup("api-target-group", { port: serverPort });
 listener.addListenerRule("api-routing", {
     conditions: [{
         pathPattern: {
